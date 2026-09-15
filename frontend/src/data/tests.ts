7abbf8cdbc38b0dd3abaf09bcc3c1d698fb68977
Do Park <do.eui.park@gmail.com>
2026-09-14 21:43:41 -0700

    update frontend/src/data/tests.ts

--- frontend/src/data/tests.ts
+++ frontend/src/data/tests.ts
@@ -1,7 +1,9 @@
 import type { WritingTest } from "../types";
 
+import topik102 from "./writing-tests/topik-102.json";
 import writingSample from "./writing-tests/writing-sample.json";
 
 export const writingTests: WritingTest[] = [
+  topik102 as WritingTest,
   writingSample as WritingTest,
 ];
