fc6e9775570b60648cc92110dc920df42fe9778d
Do Park <do.eui.park@gmail.com>
2026-09-16 19:46:25 -0700

    Add TOPIK writing tests through round 96

--- frontend/src/data/tests.ts
+++ frontend/src/data/tests.ts
@@ -1,9 +1,29 @@
 import type { WritingTest } from "../types";
 
 import topik102 from "./writing-tests/topik-102.json";
+import topik96 from "./writing-tests/topik-96.json";
+import topik91 from "./writing-tests/topik-91.json";
+import topik83 from "./writing-tests/topik-83.json";
+import topik64 from "./writing-tests/topik-64.json";
+import topik60 from "./writing-tests/topik-60.json";
+import topik52 from "./writing-tests/topik-52.json";
+import topik47 from "./writing-tests/topik-47.json";
+import topik41 from "./writing-tests/topik-41.json";
+import topik37 from "./writing-tests/topik-37.json";
+import topik36 from "./writing-tests/topik-36.json";
 import writingSample from "./writing-tests/writing-sample.json";
 
 export const writingTests: WritingTest[] = [
   topik102 as WritingTest,
+  topik96 as WritingTest,
+  topik91 as WritingTest,
+  topik83 as WritingTest,
+  topik64 as WritingTest,
+  topik60 as WritingTest,
+  topik52 as WritingTest,
+  topik47 as WritingTest,
+  topik41 as WritingTest,
+  topik37 as WritingTest,
+  topik36 as WritingTest,
   writingSample as WritingTest,
 ];
